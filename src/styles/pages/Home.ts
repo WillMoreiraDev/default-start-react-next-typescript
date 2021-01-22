321a5efd3c2845c0db7ff0ebb8f827238e67ea0e
WillMoreiraDev <willmoreiradev@gmail.com>
2021-01-22 09:21:29 -0300

    First Commit

---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    font-size: 54px;
+    background-image: linear-gradient(to right, #fb28ff, #284bff);
+    color: transparent;
+    -webkit-background-clip: text;
+    margin-top: 40px;
+  }
+
+  p {
+    margin-top: 24px;
+    font-size: 24px;
+    line-height: 32px;
+  }
+`
